8fbe3a11e846acc354f9c6f0700e0805efc783ee
jimmy nagles <jimmynagles@gmail.com>
2019-12-12 18:51:54 -0800

    axiou

--- liri.js
+++ liri.js
@@ -3,6 +3,43 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-console.log(process.argv)
 
-search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+
+// Takes in all of the command line arguments
+var inputString = process.argv;
+
+// Parses the command line argument to capture the "operand" (add, subtract, multiply, etc) and the numbers
+var action = inputString[2];
+var search = inputString[3];
+
+
+
+// Determines the operand selected...
+// Based on the operand we run the appropriate math on the two numbers
+switch (action) {
+
+  //for concert
+  case "concert-this":
+
+
+
+
+    break;
+//for spotify song
+  case "spotify-this-song":
+
+    break;
+//movies
+  case "movie-this":
+
+
+
+    break;
+//do what it says
+  case "do-what-it-says":
+
+
+
+    break;
+
+};
